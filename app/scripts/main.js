229da8abb6d6c9a2f2ee218d5f5cc5dc3c8177a4
Are Almaas <mralmaas@gmail.com>
2013-10-23 01:41:36 +0200

    Step 2 cleaned

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,3 @@
 angular.module('todoApp').controller('MainCtrl', function ($scope) {
 
-    $scope.todos = [
-        {text: 'Do something important'},
-        {text:  'Be awesome', done: true}
-    ];
-
-    $scope.add = function (todo) {
-        $scope.todos.push({text: todo});
-        $scope.newTodo = undefined;
-    };
-
-    $scope.delete = function (i) {
-        $scope.todos.splice(i, 1);
-    };
-
 });

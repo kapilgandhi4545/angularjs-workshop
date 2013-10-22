994a9523b7e778ec350eddaa6910589f6730e1f7
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-10-22 21:54:59 +0200

    Step 2 completed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,17 @@
 angular.module('todoApp').controller('MainCtrl', function ($scope) {
-   $scope.message = "Hello world!";
+
+    $scope.todos = [
+        {text: 'Do something important'},
+        {text:  'Be awesome', done: true}
+    ];
+
+    $scope.add = function (todo) {
+        $scope.todos.push({text: todo});
+        $scope.newTodo = undefined;
+    };
+
+    $scope.delete = function (i) {
+        $scope.todos.splice(i, 1);
+    };
+
 });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,17 +1,46 @@
 describe('Controller: MainCtrl', function () {
     var MainCtrl, scope;
 
-        beforeEach(module('todoApp'));
+    beforeEach(module('todoApp'));
 
-        beforeEach(inject(function ($controller, $rootScope) {
-                scope = $rootScope.$new();
-                MainCtrl = $controller('MainCtrl', {
-                        $scope: scope
-                });
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope
+        });
     }));
 
-    it('should attach a message to the scope', function () {
-            expect(scope.message).toBeDefined();
-        });
+    it('should attach an array of todos to scope', function () {
+        expect(typeof scope.todos).toBe('object');
+    });
 
+    it('should attach an add function to scope', function() {
+        expect(typeof scope.add).toBe('function');
+    });
+
+    it('should push an object with a text property onto todos i.e. scope.todos.push({text: todo})', function() {
+        scope.add('Horse');
+        expect(scope.todos.pop().text).toEqual('Horse');
+    });
+
+    it('should empty scope.newTodo on add', function() {
+        scope.newTodo = 'Horse';
+        scope.add();
+        expect(scope.newTodo).toBeUndefined();
+    });
+
+    it('should attach a delete method to scope', function() {
+        expect(typeof scope.delete).toBe('function');
+    });
+
+    it('should delete items on given index', function() {
+        scope.add('A');
+        scope.add('B');
+        scope.add('C');
+
+        var index = scope.todos.length - 1;
+        scope.delete(index);
+
+        expect(scope.todos.pop().text).toBe('B');
+    });
 })
